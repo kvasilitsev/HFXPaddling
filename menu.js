4ee7f19cae8d0ae992eccf9a95f17d9f3dc6c886
kvasilitsev <k.vasylitsev@gmail.com>
2023-02-19 14:34:22 -0400

    add responsive CSS

--- menu.js
+++ menu.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $(".btn").on("click", function(){       
+    $(".btn").on("click", function(){         //toogle menu if button onClick event  
         $(".menu").toggleClass("open-menu");
       });   
 })
 $(document).on("click", function(event){
   const currentTag = event.target.tagName;
   const isOpen = document.querySelector(".open-menu"); //check if menu is open  
-  if(currentTag === 'BODY' && isOpen){
+  if(currentTag === 'BODY' && isOpen){                 //toogle menu if <BODY> onclick event && menu is open
     $(".menu").toggleClass("open-menu");
   }
 });
